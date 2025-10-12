d3da66b029660823ed4e62fdadffefd9b06c0f7b
Learntheropes <giovanni@learntheropes.xyz>
2025-10-12 00:28:39 -0300

    redirect from naked domain to www

--- server/middleware/nakedToWww.js
+++ server/middleware/nakedToWww.js
@@ -3,13 +3,13 @@ import { sendRedirect, getRequestURL } from 'h3';
 const { public: { isDeployed } } = useRuntimeConfig();
 
 export default defineEventHandler( async event => {
-  // if (isDeployed) {
-  //   const protocol = getRequestURL(event).protocol;
-  //   const host = getRequestURL(event).host;
-  //   const path = getRequestURL(event).pathname;
-  //   if (!host.startsWith('www') &&
-  //       !path.startsWith('/api/')) {
-  //     await sendRedirect(event, `${protocol}//www.${host}${path}`);
-  //   }
-  // }
+  if (isDeployed) {
+    const protocol = getRequestURL(event).protocol;
+    const host = getRequestURL(event).host;
+    const path = getRequestURL(event).pathname;
+    if (!host.startsWith('www') &&
+        !path.startsWith('/api/')) {
+      await sendRedirect(event, `${protocol}//www.${host}${path}`);
+    }
+  }
 })
